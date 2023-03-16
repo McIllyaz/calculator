a4c4d8181d03ac33efe0a369f86fff2cad86eaa8
McIllyaz <Aryatelagasari@gmail.com>
2023-03-16 14:50:35 +0700

    menambahkan fungsi penambahan

--- calculator.cs
+++ calculator.cs
@@ -1,7 +1,14 @@
 <<<<<<< HEAD
 class calculator{
 	static void Main(string[] args){
+		int a = 10;
+		int b = 6;
 		
+		Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} - {1} = {2}", a, b, pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan apa saja untuk keluar");
+		Console.readkey();
 	}
 	
 	static int penambahan(int a, int b){
